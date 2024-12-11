eb26bc80592788412d6f565ffa3fd94d305c482e
Juan Cruz Elias <eliasjuancruz54@gmail.com>
2024-12-11 11:39:49 -0300

    Realizacion de sistema de configuracion para mobile

---
+++ app/src/main/java/com/juancruz/mysettings/SettingsActivity.kt
@@ -0,0 +1,29 @@
+package com.juancruz.mysettings
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.juancruz.mysettings.databinding.ActivitySettingsBinding
+
+class SettingsActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivitySettingsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_settings)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        binding = ActivitySettingsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+    }
+}
